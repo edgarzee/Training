60d9296d079b1980fe6ddbc8ce46927827e2b552
edgarzee <156530574+edgarzee@users.noreply.github.com>
2024-03-14 13:08:01 +0000

    Chapter 10. Started chapter 10 exercises.

---
+++ src/java_training/ppten/OutOfRangeException.java
@@ -0,0 +1,10 @@
+package java_training.ppten;
+
+public class OutOfRangeException extends Exception {
+    //---------------------------------------------------------------
+    //Sets up the exception object with a particular message
+    //---------------------------------------------------------------
+    OutOfRangeException (String message){
+        super (message);
+    }
+}
